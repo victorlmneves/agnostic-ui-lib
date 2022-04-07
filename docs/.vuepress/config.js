e4028a271565808f8f4f6a194af85131429a8399
Victor Neves <info@victorneves.dev>
2022-04-07 18:41:30 +0100

    Update packages and delete .npmrc

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,8 @@
 // const VueExamplePlugin = require('vuepress-plugin-vue-example')
+const { copyCode } = require('vuepress-plugin-copy-code2')
 const path = require('path')
 
 module.exports = {
-  bundler: '@vuepress/bundler-vite',
   title: 'DEKP Agnostic UI Library',
   themeConfig: {
     // darkMode: false,
@@ -18,21 +18,24 @@ module.exports = {
       }
     ]
   },
-  // plugins: [
-  //   [
-  //     '@vuepress/plugin-search',
-  //     {
-  //       locales: {
-  //         '/': {
-  //           placeholder: 'Search',
-  //         },
-  //       },
-  //     },
-  //   ],
-  //   // VueExamplePlugin({
-  //   //   // You need to provide a directory that all the example .vue files will be stored.
-  //   //   // You can use sub-directories to separate examples into categories.
-  //   //   componentsPath: '/docs/.examples/'
-  //   // })
-  // ],
+  plugins: [
+    [
+      '@vuepress/plugin-search',
+      {
+        locales: {
+          '/': {
+            placeholder: 'Search',
+          },
+        },
+      },
+    ],
+    copyCode({
+      // your options
+    })
+    // VueExamplePlugin({
+    //   // You need to provide a directory that all the example .vue files will be stored.
+    //   // You can use sub-directories to separate examples into categories.
+    //   componentsPath: '/docs/.examples/'
+    // })
+  ],
 }
